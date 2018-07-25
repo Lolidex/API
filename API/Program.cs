4929a9e2c223f498ff7796cd5a150fe4fdb7fb1e
Indra <wuppie55@gmail.com>
2018-07-26 01:11:14 +0200

    added basic nancyfx code

---
+++ API/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace API
+{
+    class Program
+    {
+
+        private static NancyHost _host;
+
+        static void Main(string[] args)
+        {
+            HostConfiguration hostConfiguration = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations()
+                {
+                    CreateAutomatically = true
+                }
+            };
+
+            _host = new NancyHost(hostConfiguration, new Uri("http://localhost:8888"));
+
+            _host.Start();
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                Console.WriteLine("^c pressed quiting..");
+
+                _host.Stop();
+            };
+
+            while (true) Thread.Sleep(1);
+        }
+
+    }
+}
