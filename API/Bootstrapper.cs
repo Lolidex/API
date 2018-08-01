12c9e81b61e66c0335921307fd8253e557b95213
Indra <wuppie55@gmail.com>
2018-08-01 21:24:31 +0200

    added CORS header

---
+++ API/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace API
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.AfterRequest.AddItemToEndOfPipeline((context) =>
+            {
+                context.Response.WithHeader("Access-Control-Allow-Origin", "*");
+            });
+        }
+    }
+}
